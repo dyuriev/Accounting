a7594862c592557e229eb337dc828a568d012051
Dmitriy Yuriev <coolkid00@gmail.com>
2015-08-06 02:53:39 +0300

    Interim update #3

--- js/app.js
+++ js/app.js
@@ -18,6 +18,25 @@
         return dateArr.join('.');
     }
 
+    function ObjectsStorage(storageName) {
+        this.storage = [];
+        this.storageName = storageName;
+    }
+
+    ObjectsStorage.prototype.set = function(key, value) {
+        this.storage[key] = value;
+        this.sync();
+    };
+
+    ObjectsStorage.prototype.get = function(key) {
+        return this.storage[key];
+    };
+
+    ObjectsStorage.prototype.sync = function() {
+        var jsonData = JSON.stringify(this.storage);
+        localStorage.setItem(this.storageName, jsonData);
+    };
+
 
     $(function() {
         $('div.form-group .input-group.date').datepicker({
@@ -31,4 +50,8 @@
 
         $('#date-add').val(formatDate(new Date()));
     });
+
+    var storage1 = new ObjectsStorage('some');
+    storage1.set('asdasd', {a:2});
+    console.dir(storage1.get('asdasd'));
 }());
